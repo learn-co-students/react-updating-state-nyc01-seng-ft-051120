0f5d718fb636102c86608c639d9424f71ab847ca
CMSTie <cmstie@gmail.com>
2020-07-14 15:25:49 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 
 import ClickityClick from './components/ClickityClick';
 
-ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
-  document.getElementById('root')
-);
+
+ReactDOM.render(<ClickityClick />, document.getElementById('root'));
+
+// ReactDOM.render(
+//   <div>
+//     Mount Components Here
+//   </div>,
+//   document.getElementById('root')
+// );
