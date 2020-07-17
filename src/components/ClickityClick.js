abfe9aee99c71a8fbbf3a3ad07e13472dc591c6a
JulianCedric <julian.pormentilla@gmail.com>
2020-07-17 06:29:22 -0400

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,49 @@
-// Code ClickityClick Component Here
+// State changes happen asynchronously
+// initial state:       this.state
+// updated state:       this.setState 
+
+
+
+import React from 'react';
+
+class ClickityClick extends React.Component {
+
+// constructor() is like 'initialize' in Ruby Classes 
+    constructor() {
+        super();
+
+// Define the initial state:
+
+        this.state = {
+        hasBeenClicked: false
+        };
+    }
+
+// Update our state here...
+
+    handleClick = () => {
+        this.setState({
+        hasBeenClicked: true
+        }, () => 
+        console.log(this.state.hasBeenClicked)) // prints true
+  }
+
+    // handleClick = () => {
+    //     this.setState({
+    //     hasBeenClicked: true
+    //     })
+    //     console.log(this.state.hasBeenClicked); // prints false 
+    // }
+
+///////
+
+    render() { 
+        return (  
+            <div>
+                <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+                <button onClick={this.handleClick}>Click me!</button>
+            </div>
+        );
+    }
+}
+ export default ClickityClick;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+ 
+import ClickityClick from './components/ClickityClick';
+import JulianCounter from './components/JulianCounter.js';
+ 
 ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
-  document.getElementById('root')
-);
+  <ClickityClick />, 
+  // <JulianCounter />,
+document.getElementById('root'));
